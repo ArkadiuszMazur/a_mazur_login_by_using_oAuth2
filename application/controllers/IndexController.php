e3489b15e7330d8631e7e49d5efa34d930e9672c
amazur <arek.mazur@o2.pl>
2017-01-13 19:04:56 +0100

    New action/view and css/js path problem resolved

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,9 +6,23 @@ class IndexController extends GlobalController {
         /* Initialize action controller here */
     }
 
-    public function indexAction() {         
+    public function indexAction() {               
         $model = new Model_appModel();        
-        $model->test();
+        //$model->test();
+        
+        /**
+         * Identyfikator projektu będzie mieć wartość amazur-155511
+         * 
+         * Identyfikator klienta:
+         * 689383590563-fr0gsklufm984eom1451g33ou7fdn448.apps.googleusercontent.com
+         * 
+         * Tajny klucz klienta:
+         * Zux_BXaN1AbZBGMnCbI8bykH
+         */
     }
 
+    
+    public function signinAction() {
+        
+    }
 }
