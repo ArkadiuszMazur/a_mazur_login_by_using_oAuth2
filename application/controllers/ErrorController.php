2f0b134b5fa6a1d31d0797fa1fc18c92a9a1b5d0
amazur <arek.mazur@o2.pl>
2017-01-15 14:09:01 +0100

    login_error action

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -2,11 +2,19 @@
 
 class ErrorController extends GlobalController {
 
+    protected $_config;
+    
+    public function init() {
+        $ds = DIRECTORY_SEPARATOR;
+        $this->_config = new Zend_Config_Ini(APPLICATION_PATH . $ds . 'configs' . $ds . 'application.ini', 'production');
+        parent::init();
+    }
+
     /**
      * Default Zend error actin
      */
     public function errorAction() {
-        $errors = $this->_getParam('error_handler');        
+        $errors = $this->_getParam('error_handler');
 
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
@@ -26,22 +34,32 @@ class ErrorController extends GlobalController {
         $this->view->exception = $errors->exception;
         $this->view->request = $errors->request;
     }
-    
+
     /**
      * Communication error via googleApi
      */
-    public function comerrorAction() {        
-        $this->_helper->layout->setLayout('error');                
-        $this->view->errorText = 'I`m sorry, an error occured during communication with google API.';        
+    public function comerrorAction() {
+        $this->_helper->layout->setLayout('error');
+        $this->view->errorText = 'I`m sorry, an error occured during communication with google API.';
     }
-    
+
     /*
      * Response error
      */
-    public function reserrorAction() {        
-        $this->_helper->layout->setLayout('error');                
+
+    public function reserrorAction() {
+        $this->_helper->layout->setLayout('error');
         $this->view->errorText = 'I`m sorry, response from API was not retrieved..';
-        
+    }
+
+    public function logerrorAction() {
+        if ($this->_sess->account == $this->_config->authorizedUser) {
+            $this->_flashMessenger->addMessage('You have been logged properly!');
+            $this->_helper->redirector->gotoUrl('/');
+        }
+
+        $this->_helper->layout->setLayout('error');
+        $this->view->errorText = 'Your currently account is: ' . $this->_sess->account . '. You need to be logged by account amazur.edukey@gmail.com. Please use password: nx.media.pl';
     }
 
 }
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,53 +2,53 @@
 
 class IndexController extends GlobalController {
 
-    public function init() {        
+    public function init() {
         parent::init();
     }
 
     public function indexAction() {
-        if(!$this->_sess->logged) {
-            $this->_helper->redirector->gotoUrl('/signin'); 
+        if (!$this->_sess->logged) {
+            $this->_helper->redirector->gotoUrl('/signin');
         }
-        $model = new Model_appModel();                    
+        $model = new Model_appModel();
     }
 
-    public function signinAction() {           
-        if($this->_sess->logged) {
-            $this->_helper->redirector->gotoUrl('/');  
-        }                
+    public function signinAction() {
+        if ($this->_sess->logged) {
+            $this->_helper->redirector->gotoUrl('/');
+        }
         $ds = DIRECTORY_SEPARATOR;
-        $config = new Zend_Config_Ini(APPLICATION_PATH . $ds . 'configs' . $ds . 'application.ini', 'production');                               
-        $googleConfigCurrentDomain = $config->google->configCurrentDomain;                 
-        $model = new Model_googleapiModel($googleConfigCurrentDomain);       
-        
-        $getState = $this->_getParam('state');   
-        $getCode = $this->_getParam('code');   
-        
+        $config = new Zend_Config_Ini(APPLICATION_PATH . $ds . 'configs' . $ds . 'application.ini', 'production');
+        $googleConfigCurrentDomain = $config->google->configCurrentDomain;
+        $model = new Model_googleapiModel($googleConfigCurrentDomain);
+
+        $getState = $this->_getParam('state');
+        $getCode = $this->_getParam('code');
+
         /**
          * First request - we redirect to accounts.google.com
          */
         if (!$getCode || !$getState) {
-            $this->_sess->uniqueId = uniqid();           
-            $accountsUrl = $model->getAccountsUrl($this->_sess->uniqueId, 'email');                        
-            $this->_helper->redirector->gotoUrl($accountsUrl);            
+            $this->_sess->uniqueId = uniqid();
+            $accountsUrl = $model->getAccountsUrl($this->_sess->uniqueId, 'email');
+            $this->_helper->redirector->gotoUrl($accountsUrl);
         }
-        
+
         /**
          * If communication with API problem occured
          */
-        if($getState != $this->_sess->uniqueId) {                        
+        if ($getState != $this->_sess->uniqueId) {
             $this->_helper->redirector->gotoUrl('/communication_error');
         }
-                
-        $response = $model->retrieveData();    
+
+        $response = $model->retrieveData();
         /**
          * No response case 
          */
-        if(is_array($response) && empty($response)) { //am_t przywrócić i usunąć poniższe                
+        if (is_array($response) && empty($response)) { //am_t przywrócić i usunąć poniższe                
             $this->_helper->redirector->gotoUrl('/response_error');
         }
-        
+
         $response = json_decode($response, true);
 
         $accountEmail = isset($response['email']) ? trim($response['email']) : null;
@@ -58,26 +58,31 @@ class IndexController extends GlobalController {
         if (!$accountEmail) {
             // tutaj np. przekierowanie, jeżeli nie udało się pobrać danych konta Google
         }
-        
-        if(trim($config->authorizedUser) == $accountEmail) {
+
+        $this->_sess->account = $accountEmail;
+        if (trim($config->authorizedUser) == $accountEmail) {
             $this->_sess->logged = true;
             $this->_sess->loginProcessing = false;
             $this->_flashMessenger->addMessage('You have been logged!');
-            $this->_helper->redirector->gotoUrl('/'); 
-        }                         
+            $this->_helper->redirector->gotoUrl('/');
+        } else {                        
+            $this->_helper->redirector->gotoUrl('/login_error');
+        }
     }
-    
+
     /*
      * Here you can log out from application.
      * But if you want to really be logged out, you should also logged out from google account,
      * because the signinAction will automaticaly check your log in to google account.
      */
+
     public function logoutAction() {
         $this->_sess->logged = false;
-        $this->_sess->loginProcessing = false;            
-        $this->_helper->redirector->gotoUrl('/'); 
+        $this->_sess->loginProcessing = false;
+        unset($this->_sess->account);
+        $this->_helper->redirector->gotoUrl('/');
     }
-        
+
     /**
      * After the action, before view is rendering.
      */
